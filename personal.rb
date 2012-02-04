2950436c9fbdb6c9888f4e8973356ba86f1baaab
Thomas Cioppettini <thomas.cioppettini@gmail.com>
2012-02-03 19:24:35 -0500

    Configuring email for sendgrid

--- personal.rb
+++ personal.rb
@@ -8,7 +8,27 @@ get '/' do
 end
 
 post '/' do 
-  
+  name = params[:name]
+  sender_email = params[:email]
+  message = params[:message]
+  require 'pony'
+       Pony.mail(
+        :from => "#{name} <#{sender_email}>",
+        :to => 'thomas.cioppettini@gmail.com',
+        :subject =>"#{name} has contacted you",
+        :body => "#{message}",
+        :port => '587',
+        :via => :smtp,
+        :via_options => { 
+          :address              => 'smtp.sendgrid.net', 
+          :port                 => '587', 
+          :enable_starttls_auto => true, 
+          :user_name            => ENV['SENDGRID_USERNAME'], 
+          :password             => ENV['SENDGRID_PASSWORD'], 
+          :authentication       => :plain, 
+          :domain               => ENV['SENDGRID_DOMAIN']
+        })
+      redirect '/success'
 end
 
 get '/success' do
