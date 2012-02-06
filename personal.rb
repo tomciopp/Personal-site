53671f7ceb6b1d871e07f0746b67dc532987d26f
Thomas Cioppettini <thomas.cioppettini@gmail.com>
2012-02-06 01:59:26 -0500

    playing around with email contact form

--- personal.rb
+++ personal.rb
@@ -3,6 +3,12 @@ require 'sinatra'
 require 'pony'
 require 'sinatra/content_for'
 
+set :email_username, ENV['SENDGRID_USERNAME'] ||
+set :email_password, ENV['SENDGRID_PASSWORD'] ||
+set :email_address, 'thomas.cioppettini@gmail.com'
+set :email_service, ENV['EMAIL_SERVICE'] || 'gmail.com'
+set :email_domain, ENV['SENDGRID_DOMAIN'] || 'localhost.localdomain'
+
 helpers Sinatra::ContentFor
 
 get '/' do 
@@ -26,7 +32,7 @@ post '/contact' do
         :port                 => '587',  
         :user_name            => ENV['SENDGRID_USERNAME'], 
         :password             => ENV['SENDGRID_PASSWORD'], 
-        :domain               => 'heroku.com',
+        :domain               => ENV['SENDGRID_DOMAIN'],
         :authentication       => :plain, 
         :enable_starttls_auto => true
       })
