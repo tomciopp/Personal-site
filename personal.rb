1bf4ad62dcf46da9fc5b42af31a6b98f9850e0cf
Thomas Cioppettini <thomas.cioppettini@gmail.com>
2012-02-06 19:33:30 -0500

    Checking syntax error

--- personal.rb
+++ personal.rb
@@ -14,22 +14,23 @@ post '/contact' do
   name = params[:name]
   sender_email = params[:email]
   message = params[:message]
-    Pony.mail(
-      :from => "#{name}<#{sender_email}>",
-      :to => 'thomas.cioppettini@gmail.com',
-      :subject =>"#{name} has contacted you",
-      :body => "#{message}",
-      :port => '587',
-      :via => :smtp,
-      :via_options => { 
-        :address              => 'smtp.sendgrid.net', 
-        :port                 => '587',  
-        :user_name            => ENV['SENDGRID_USERNAME'], 
-        :password             => ENV['SENDGRID_PASSWORD'], 
-        :domain               => 'heroku.com',
-        :authentication       => :plain, 
-        :enable_starttls_auto => true
-      })
+  log params.inspect
+  Pony.mail(
+    :from => "#{name}<#{sender_email}>",
+    :to => 'thomas.cioppettini@gmail.com',
+    :subject =>"#{name} has contacted you",
+    :body => "#{message}",
+    :port => '587',
+    :via => :smtp,
+    :via_options => { 
+      :address              => 'smtp.sendgrid.net', 
+      :port                 => '587',  
+      :user_name            => ENV['SENDGRID_USERNAME'], 
+      :password             => ENV['SENDGRID_PASSWORD'], 
+      :domain               => ENV['SENDGRID_DOMAIN'],
+      :authentication       => :plain, 
+      :enable_starttls_auto => true
+    })
     redirect '/success'
 end
 
